593fbc3f936a604e1c9a87b91de98e7aae31c215
Justin <justflyn1@gmail.com>
2021-09-04 15:11:17 -0400

    Fix ordering of GUI elements

--- RougelikeConsole/src/Main.java
+++ RougelikeConsole/src/Main.java
@@ -6,17 +6,17 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
-        //JFrame window settings
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //So program actually stops when X is clicked
-        frame.setSize(300,300);
-        frame.setLayout(null);
-        frame.setVisible(true);
-
         //Map out text pane
         JTextPane textPane = new JTextPane();
         textPane.setEditable(false);
         textPane.setBounds(0,0,300,300);
         textPane.setBackground(Color.BLACK);
         frame.add(textPane);
+
+        //JFrame window settings - has to happen last
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //So program actually stops when X is clicked
+        frame.setSize(320,350);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
