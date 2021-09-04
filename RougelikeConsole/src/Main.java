2c7be864f2901ecc5cd4094c03417bed586408ba
Justin <justflyn1@gmail.com>
2021-09-04 14:57:10 -0400

    Add basic GUI foundation

--- RougelikeConsole/src/Main.java
+++ RougelikeConsole/src/Main.java
@@ -1,6 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame = new JFrame();
+
+        //JFrame window settings
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //So program actually stops when X is clicked
+        frame.setSize(300,300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        //Map out text pane
+        JTextPane textPane = new JTextPane();
+        textPane.setEditable(false);
+        textPane.setBounds(0,0,300,300);
+        textPane.setBackground(Color.BLACK);
+        frame.add(textPane);
     }
 }
