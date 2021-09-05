6ef61851f0975a3f752bad5cf5c169810c33183c
nethegre <thegaming4official@gmail.com>
2021-09-05 16:53:06 -0400

    Created the MeleAttackUse component.

--- RoguelikeConsole/src/component/Use.java
+++ RoguelikeConsole/src/component/Use.java
@@ -1,11 +1,15 @@
 package component;
 
 import base.Base;
+import base.Character;
+import base.Entity;
 import base.Item;
+
+import java.util.List;
 
 public abstract class Use extends Base {
 
-    public void use(Item i)
+    public void use(Character character, Item i, int direction, List<Entity> gameEntityList)
     {
 
     }
